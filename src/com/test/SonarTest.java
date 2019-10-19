ae2ab1d42044f9c1c52c0c4bac95479c028c9ded
Bhardwaj <amit.a.bhardwaj@capgemini.com>
2019-10-19 16:19:28 +0530

    First App

---
+++ src/com/test/SonarTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import java.util.*;
+import java.lang.annotation.*;
+
+public class SonarTest {
+	String str;
+	public static void main(String[] args) {
+		System.out.println("Hello Jenkins");
+	}
+
+}
